f4a63cee27e71afe010fc26d54a50b8687a78f4b
JesusTalayeroP <jesus_talayero_apps1ma1920@cev.com>
2021-01-28 14:11:01 +0100

    Initial Commit

---
+++ Agenda2077/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Agenda2077
+//
+//  Created by alumnos on 28/01/2021.
+//  Copyright © 2021 cev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Agenda2077Tests/Agenda2077Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Agenda2077Tests.swift
+//  Agenda2077Tests
+//
+//  Created by alumnos on 28/01/2021.
+//  Copyright © 2021 cev. All rights reserved.
+//
+
+import XCTest
+@testable import Agenda2077
+
+class Agenda2077Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
